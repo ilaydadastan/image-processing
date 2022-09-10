eff14aa8d39862615f14cebab7980edb8990d7c0
ilaydadastan <ilaydastan2@gmail.com>
2022-09-11 01:40:33 +0300

    addition image

---
+++ arithmetic_operations/addition_image.py
@@ -0,0 +1,13 @@
+import cv2
+import numpy as np
+
+image1 = cv2.imread('road.jpeg')
+image2 = cv2.imread('image1.jpeg')
+
+
+weightedSum = cv2.addWeighted(image1, 0.5, image2, 0.4, 0)  # ???
+
+cv2.imshow('Weighted Image', weightedSum)
+
+if cv2.waitKey(0):
+    cv2.destroyAllWindows()
