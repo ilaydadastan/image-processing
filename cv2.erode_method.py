f64fbae305c23b76742318375dac71f900c9b074
ilaydadastan <ilaydastan2@gmail.com>
2022-09-12 20:58:10 +0300

    added erode method

---
+++ cv2.erode_method.py
@@ -0,0 +1,16 @@
+import cv2
+import numpy as np
+
+path = r'/Users/ilaydadastan/PycharmProjects/image-processing/road.jpeg'
+
+image = cv2.imread(path)
+
+window_name = 'image'
+
+kernel = np.ones((7, 7""), np.uint8)
+
+image = cv2.erode(image, kernel)
+
+cv2.imshow(window_name, image)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
